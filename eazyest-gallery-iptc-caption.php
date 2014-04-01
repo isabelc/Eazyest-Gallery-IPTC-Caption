e340932a35c5c0ecb7a6261853b1289ac08be1a0
isabelc <me@isabelcastillo.com>
2014-04-01 00:49:45 -0400

    add GPL header

--- eazyest-gallery-iptc-caption.php
+++ eazyest-gallery-iptc-caption.php
@@ -9,6 +9,21 @@ Author URI: http://isabelcastillo.com
 License: GPL2
 Text Domain: eazyest-gallery-iptc-caption
 Domain Path: languages
+*
+* Copyright 2014 Isabel Castillo
+
+* Eazyest Gallery IPTC Caption is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* any later version.
+*
+* Eazyest Gallery IPTC Caption is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Eazyest Gallery IPTC Caption. If not, see <http://www.gnu.org/licenses/>.
 */
 
 class Eazyest_Gallery_IPTC_Caption {
